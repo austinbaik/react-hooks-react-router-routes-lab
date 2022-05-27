bb0ffc17c52d17807b5d2c042489a2e300afd55e
Austin <baik.austin@gmail.com>
2022-05-27 09:54:33 -0700

    commit tree 37dc840c4437ac129b6fb372d542393ae0504d47

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,62 @@
 import React from "react";
 import { movies } from "../data";
 
+
+// {
+//   title: "Doctor Strange",
+//   time: 115,
+//   genres: ["Action", "Adventure", "Fantasy"],
+// },
+
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  // const renderedMovies = movies.map(movie => movie.genres.map(genre =>{
+
+
+
+  //   return (
+  //     <div>
+  //       <h2>Name: {movie.title}</h2>
+
+  //       Time: {movie.time}
+
+  //       <ul>Genres: {genre}</ul>
+
+  //     </div>
+
+
+  //   )
+  // }))
+
+
+  const renderedMovies = movies.map(movie => {
+    const renderedGenre = movie.genres.map(genre => <li key={genre}>Genres: {genre}</li>)
+
+    return (
+      <div key={movie.name}>
+        <h2>Name: {movie.title}</h2>
+        Time: {movie.time}
+        {renderedGenre}
+
+
+      </div>
+
+
+    )
+  })
+
+
+
+
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {renderedMovies}
+    </div>
+
+  )
 }
 
 export default Movies;
